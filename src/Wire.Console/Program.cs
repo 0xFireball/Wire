d8b3d469c303a0d2ceeaaee5c94af6a9dc49c2aa
0xFireball <0xFireball@outlook.com>
2017-09-21 13:12:58 -0700

    create project

---
+++ src/Wire.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wire.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Wire/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Wire
+{
+    public class Class1
+    {
+    }
+}
